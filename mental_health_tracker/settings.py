0047d0d8739b26a4caf2653c2abc1b86d01af602
salsaaruum <tatasalsa04@gmail.com>
2024-09-14 22:27:50 +0700

    Melakukan deploy ulang karena project dihapus di PWS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/web/project/salsabila.arumdapta/mentalhealthtracker2", "https://pbp.cs.ui.ac.id/web/project/salsabila.arumdapta/mentalhealthtracker2"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/web/project/salsabila.arumdapta/mentalhealthtracker", "https://pbp.cs.ui.ac.id/web/project/salsabila.arumdapta/mentalhealthtracker"]
